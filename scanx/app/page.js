535e3b0daa6d6d71f007eaf7c7ce75151d692b08
Naman Developer <namandeveloper7030@gmail.com>
2025-01-20 18:54:50 +0530

    Shadcn configured

--- scanx/app/page.js
+++ scanx/app/page.js
@@ -1,9 +1,15 @@
+import { Card } from "@/components/ui/card";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <p>Hello</p>
+      <p>Shree Ganesh ai namah</p>
+
+      <Card width = "50%">
+        Hello
+        <button>Click</button>
+      </Card>
     </div>
   );
 }
