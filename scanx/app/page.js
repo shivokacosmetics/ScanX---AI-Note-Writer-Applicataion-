d1dea6abb44c8fe34344f760765a2175f4de4d52
Naman Developer <namandeveloper7030@gmail.com>
2025-01-21 08:37:23 +0530

    update

--- scanx/app/page.js
+++ scanx/app/page.js
@@ -1,16 +1,40 @@
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+"use client";
+import { Button } from "./../components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import { api } from "./../convex/_generated/api"; 
+import { useEffect } from "react";  
+
 import Image from "next/image";
 
 export default function Home() {
+
+  const {user} = useUser();
+  const createUser = useMutation(api.user.createUser);
+
+
+  useEffect(() => {
+    if (user) {
+      user&&CheckUser();
+    }
+  }, [user]);
+  const CheckUser = async()=>{
+    const result = await createUser({
+      email:user?.primaryEmailAddress?.emailAddress,
+      imageUrl:user?.imageUrl,
+      userName:user?.fullName
+    });
+    console.log(result);
+  }
   return (
     <div>
       <p>Shree Ganesh ai namah</p>
 
-      <Card width = "50%">
+      
+        <UserButton/>
         Hello
         <Button>Click</Button>
-      </Card>
+      
     </div>
   );
 }
---
+++ scanx/convex/user.js
@@ -0,0 +1,28 @@
+import { mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+export const createUser = mutation({
+  args: {
+    email: v.string(),
+    userName: v.string(),
+    imgUrl: v.string(),
+  },
+  handler:async(ctx,args)=>{
+    //if user already exists
+
+    const user = await ctx.db.query('users').filter((q)=>q.eq(q.field('email'),args.email))
+    .collect();
+
+    if(user?.length==0){
+        await ctx.db.insert('users',{
+            email:args.email,
+            userName:args.userName,
+            imgUrl:args.imgUrl
+        });
+        return 'inserted new User...';
+    }
+
+    //if user does not exist, create a new user in your database and return the user object
+    return 'User already exists...'
+  }
+});
