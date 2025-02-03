8d9b42a21f8f13eeb1fdc71596d7e3b5e6aef7b6
Ayushi Thumar <165537687+AThumar@users.noreply.github.com>
2025-02-03 21:39:19 +0530

    workspace created with header

---
+++ scanx/app/workspace/[fileid]/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Workspace() {
+  const { fileid } = useParams();
+  return (
+    <div>
+      <h1>Workspace</h1>
+    </div>
+  );
+}
+
+export default Workspace;
