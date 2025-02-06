fcec5bf299c45e7ab37015f055272fa54173281d
Ayushi Thumar <165537687+AThumar@users.noreply.github.com>
2025-02-06 08:08:46 +0530

    unhandled runtime error

--- scanx/app/workspace/[fileid]/page.js
+++ scanx/app/workspace/[fileid]/page.js
@@ -1,17 +1,39 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "next/navigation";
-
 import Workspace_header from "./_components/workspace_header";
+import PdfViewer from "./_components/PdfViewer";
+import { useQueries } from "convex/react";
+import { v } from "convex/values";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { query } from "../../convex/_generated/../_generated/server";
+// import { query } from "./_generated/server";
+
+// export const GetFileRecord = query(async ({ db }, { fileId }) => {
+//   return await db.get(fileId);
+// });
+export const GetFileRecord = query(async ({ db }, { fileId }) => {
+  return await db.get(fileId);
+});
 
 function Workspace() {
   const { fileid } = useParams();
+  const fileInfo = useQuery(api.fileStorage.GetFileRecord, {
+    fileId: fileid,
+  });
+  useEffect(() => {
+    console.log(fileInfo);
+  }, [fileInfo]);
   return (
     <div>
       <Workspace_header />
       <div>
-        <div>{/*Text Ediotr*/}</div>
-        <div>{/*PDF Viewer*/}</div>
+        <div>{/*Text Editor*/}</div>
+        <div>
+          {/*PDF Viewer*/}
+          <PdfViewer />
+        </div>
       </div>
     </div>
   );
--- scanx/convex/pdfStorage.js
+++ scanx/convex/pdfStorage.js
@@ -1,38 +1,51 @@
 import { v } from "convex/values";
 import { mutation } from "./_generated/server";
+import { query } from "./_generated/server";
 
 export const generateUploadUrl = mutation(async (ctx) => {
   return await ctx.storage.generateUploadUrl();
 });
 
 export const AddFileEntryToDb = mutation({
-  args:{
-    fileId:v.string(),
-    storageId:v.string(),
-    fileName:v.string(),
-    createBy:v.string(),
-    fileUrl:v.string(),
+  args: {
+    fileId: v.string(),
+    storageId: v.string(),
+    fileName: v.string(),
+    createBy: v.string(),
+    fileUrl: v.string(),
   },
-  handler:async(ctx,args)=>{
-    const result = await ctx.db.insert('pdfFiles',{
-      fileId:args.fileId,
-      storageId:args.storageId,
-      fileName:args.fileName,
-      createBy:args.createBy,
-      fileUrl:args.fileUrl,  //add fileUrl column to pdfFiles table and update this field in this mutation.  This field will store the URL of uploaded file.  You can fetch this URL using getFileUrl mutation.  This URL can be used to display the uploaded file in frontend.  You can use any library or service to fetch the file from this URL.  For example, you can use Axios to fetch the file and display it in a <img> tag
-      
-    })
-    return 'inserted';
-  }
-})
+  handler: async (ctx, args) => {
+    const result = await ctx.db.insert("pdfFiles", {
+      fileId: args.fileId,
+      storageId: args.storageId,
+      fileName: args.fileName,
+      createBy: args.createBy,
+      fileUrl: args.fileUrl, //add fileUrl column to pdfFiles table and update this field in this mutation.  This field will store the URL of uploaded file.  You can fetch this URL using getFileUrl mutation.  This URL can be used to display the uploaded file in frontend.  You can use any library or service to fetch the file from this URL.  For example, you can use Axios to fetch the file and display it in a <img> tag
+    });
+    return "inserted";
+  },
+});
 
-export const getFileUrl=mutation({
-  args:{
-    storageId:v.string(),
+export const getFileUrl = mutation({
+  args: {
+    storageId: v.string(),
   },
-  handler:async(ctx,args)=>{
+  handler: async (ctx, args) => {
     const url = await ctx.storage.getUrl(args.storageId);
     return url;
-  }
+  },
+});
 
-})
+export const GetFileRecord = query({
+  args: {
+    fileId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const result = await ctx.db
+      .query("pdfFiles")
+      .filter((q) => q.eq(q.field("fileId"), args.fileId))
+      .collect();
+    console.log(result);
+    return result;
+  },
+});
