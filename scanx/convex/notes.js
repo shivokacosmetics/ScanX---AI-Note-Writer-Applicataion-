d0bcb56266fd0e3aa4099274f5a32f6e9c100448
Ayushi Thumar <165537687+AThumar@users.noreply.github.com>
2025-03-10 18:26:42 +0530

    Implement handler for notes mutation to create or update notes based on fileId

--- scanx/convex/notes.js
+++ scanx/convex/notes.js
@@ -6,4 +6,23 @@ export const notes = mutation({
     note: v.any(),
     createBy: v.string(),
   },
+  handler: async (ctx, args) => {
+    const recordId = await ctx.db
+      .query("notes")
+      .filter((q) => q.eq(q.field("fileId"), args.fileId))
+      .collect();
+
+    if (recordId.length == 0) {
+      await ctx.db.query("notes", {
+        fileId: args.fileId,
+        notes: args.note,
+        createBy: args.createBy,
+      });
+    } else {
+      await ctx.db.patch(record[0].id, {
+        notes: args.notes,
+        //createBy: args.createBy,
+      });
+    }
+  },
 });
